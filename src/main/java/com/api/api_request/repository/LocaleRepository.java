e37b57934a2726381d20ded03ad6d28502e66956
francesco-s <francescosannicola1997@gmail.com>
2020-12-24 18:54:02 +0100

    API for price filtering and Id locale filtering

--- src/main/java/com/api/api_request/repository/LocaleRepository.java
+++ src/main/java/com/api/api_request/repository/LocaleRepository.java
@@ -1,10 +1,7 @@
 package com.api.api_request.repository;
 
-<<<<<<< HEAD
 import java.util.List;
 import java.util.Optional;
-=======
->>>>>>> ea7f51b8be7fc4d16441add1ced3433d368a1522
 
 import com.api.api_request.entity.Bevanda;
 import com.api.api_request.entity.Locale;
@@ -18,15 +15,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-<<<<<<< HEAD
 public interface LocaleRepository extends JpaRepository<Locale, Integer> {
-=======
-public interface LocaleRepository extends JpaRepository<Locale, Integer>
-{
-    //@Query
-    //public List<Bevanda> findAllByDescriptionQuery(Integer id);
-    //public List<Bevanda> findByBevande_Id(Integer id);
->>>>>>> ea7f51b8be7fc4d16441add1ced3433d368a1522
 
     // @Query("SELECT name FROM Locale l JOIN FETCH l.bevande Bevanda");
     // List<Bevanda> findBevandaAsB();
